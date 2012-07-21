1d1d8174b59ae87dd13c6e71a1c2f41ab38d5434
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-07-21 10:45:46 +0700

    Initial commit

---
+++ lib/Task/BeLike/SHARYANTO/Spec.pm
@@ -0,0 +1,25 @@
+package Task::BeLike::SHARYANTO::Spec;
+
+# VERSION
+
+=head1 DESCRIPTION
+
+This task will install all SHARYANTO's spec distributions, i.e. those that
+contain only specification and not implementation.
+
+=pkgroup Included modules
+
+=pkg Riap
+
+=pkg Rinci
+
+=pkg Sah
+
+=pkg Setup
+
+=pkg Unixish
+
+=cut
+
+1;
+# ABSTRACT: SHARYANTO's spec distributions
